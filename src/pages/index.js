a72637e998a043bd3bd34628dba1c677e69e4381
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-03-31 23:40:33 +0600

    🎆Working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import * as React from "react";
 import "../styles/global.css";
+import Image1 from "./1.svg";
+import Image2 from "./2.svg";
+import Image3 from "./3.svg";
+import Image4 from "./4.svg";
+import Image5 from "./5.svg";
+import Image6 from "./6.svg";
+import Image7 from "./7.svg";
+import Image8 from "./8.svg";
+import Image9 from "./9.svg";
+import Image10 from "./10.svg";
+import Image11 from "./11.svg";
 
 const IndexPage = () => {
     return (
         <main>
-            <h1>Hasib Al Rashid</h1>
+            <div class="parallax"></div>
         </main>
     );
 };
