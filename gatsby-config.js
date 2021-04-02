0280accb41a9eb02e34b5fabb3268284c06c6851
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-02 13:29:49 +0600

    ✅Done the Front Page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
-  siteMetadata: {
-    title: "hasib-rashid",
-  },
-  plugins: ["gatsby-plugin-netlify-cms"],
+    siteMetadata: {
+        title: "hasib-rashid",
+    },
+    plugins: [
+        "gatsby-plugin-netlify-cms",
+        "gatsby-plugin-image",
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /assets/,
+                },
+            },
+        },
+    ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,72 @@
 import * as React from "react";
 import "../styles/global.css";
-import Image1 from "./1.svg";
-import Image2 from "./2.svg";
-import Image3 from "./3.svg";
-import Image4 from "./4.svg";
-import Image5 from "./5.svg";
-import Image6 from "./6.svg";
-import Image7 from "./7.svg";
-import Image8 from "./8.svg";
-import Image9 from "./9.svg";
-import Image10 from "./10.svg";
-import Image11 from "./11.svg";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
     return (
         <main>
-            <div class="parallax"></div>
+            <div className="parallax">
+                <StaticImage
+                    className="layer p1"
+                    src="../../assets/svg/1.svg"
+                    alt="Svg 1"
+                />
+                <StaticImage
+                    className="layer p2"
+                    src="../../assets/svg/2.svg"
+                    alt="Svg 2"
+                />
+                <StaticImage
+                    className="layer p3"
+                    src="../../assets/svg/3.svg"
+                    alt="Svg 3"
+                />
+                <StaticImage
+                    className="layer p4"
+                    src="../../assets/svg/4.svg"
+                    alt="Svg 4"
+                />
+                <StaticImage
+                    className="layer p5"
+                    src="../../assets/svg/5.svg"
+                    alt="Svg 5"
+                />
+                <StaticImage
+                    className="layer p6"
+                    src="../../assets/svg/6.svg"
+                    alt="Svg 6"
+                />
+                <StaticImage
+                    className="layer p7"
+                    src="../../assets/svg/7.svg"
+                    alt="Svg 7"
+                />
+                <StaticImage
+                    className="layer p8"
+                    src="../../assets/svg/8.svg"
+                    alt="Svg 8"
+                />
+                <StaticImage
+                    className="layer p9"
+                    src="../../assets/svg/9.svg"
+                    alt="Svg 9"
+                />
+                <StaticImage
+                    className="layer p10"
+                    src="../../assets/svg/10.svg"
+                    alt="Svg 10"
+                />
+                <StaticImage
+                    className="layer p11"
+                    src="../../assets/svg/11.svg"
+                    alt="Svg 11"
+                />
+                <div className="infos">
+                    <h1>Hasib Al Rashid</h1>
+                    <h2>Programmer, Student, Cadet</h2>
+                    <h3>13 Years Old</h3>
+                </div>
+            </div>
         </main>
     );
 };
