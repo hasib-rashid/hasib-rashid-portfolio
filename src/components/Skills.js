e10e28b2617d44efc991525456b35fe575f03555
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-04 23:29:26 +0600

    🤹‍♀️Done with MySkills!

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,10 +17,9 @@ const Skills = () => {
 
                     <p>ReactJS</p>
                 </div>
-
                 <div className="skill">
                     <StaticImage
-                        src="../../assets/icon/gatsby.svg"
+                        src="https://seeklogo.com/images/G/gatsby-logo-1A245AD37F-seeklogo.com.png"
                         alt="React Icon"
                         width="155px"
                         height="50px"
@@ -28,16 +27,62 @@ const Skills = () => {
 
                     <p>Gatsby</p>
                 </div>
-
                 <div className="skill">
                     <StaticImage
                         src="https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png"
                         alt="NextJS Icon"
-                        width="150px"
+                        width="155px"
                         height="125px"
                     />
 
                     <p>NextJS</p>
+                </div>
+                <div className="skill">
+                    <StaticImage
+                        src="https://img.icons8.com/fluent/125/000000/discord-new-logo.png"
+                        alt="Discord Icon"
+                        width="155px"
+                        height="125px"
+                    />
+
+                    <p>DiscordJS</p>
+                </div>
+                <div className="skill">
+                    <StaticImage
+                        src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_logo_icon_146424.png"
+                        alt="MongoDB Icon"
+                        width="155px"
+                        height="125px"
+                    />
+
+                    <p>MongoDB</p>
+                </div>
+
+                <div className="skill">
+                    <StaticImage
+                        src="https://img.icons8.com/color/125/000000/firebase.png"
+                        alt="Firebase Icon"
+                    />
+
+                    <p>Firebase</p>
+                </div>
+
+                <div className="skill">
+                    <StaticImage
+                        src="https://img.icons8.com/color/125/000000/css3.png"
+                        alt="CSS Icon"
+                    />
+
+                    <p>CSS</p>
+                </div>
+
+                <div className="skill">
+                    <StaticImage
+                        src="https://img.icons8.com/color/125/000000/nodejs.png"
+                        alt="NodeJS Icon"
+                    />
+
+                    <p>NodeJS</p>
                 </div>
             </div>
         </div>
