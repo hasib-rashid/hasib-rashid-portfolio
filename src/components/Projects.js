fca95bfd8d64e0d584dc7a6dd8012a5d18daac12
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-05 23:50:41 +0600

    🍀Project Card Modeling Done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Projects = () => {
     return (
@@ -6,9 +7,17 @@ const Projects = () => {
             <h1>My Projects</h1>
 
             <div className="project">
-                <h2>Project</h2>
+                <StaticImage
+                    src="http://localhost:8000/static/810c4a65d51355568a4dd50cdf9e9a02/0cff0/react.webp"
+                    alt="Project Img"
+                    className="project_img"
+                />
 
-                <p>This is my Project</p>
+                <div className="project_txt">
+                    <h2>ReactJS Project</h2>
+
+                    <p>This is my ReactJS project Bla blah blah</p>
+                </div>
             </div>
         </div>
     );
